f31cbb3e42f9bb3b3d3a624a3ed1d797bc8029e2
emanuela012 <emanuela.matei@student.upt.ro>
2024-12-11 19:31:53 +0200

    commit no2

--- OrderManagement.API/obj/Debug/net8.0/OrderManagement.API.AssemblyInfo.cs
+++ OrderManagement.API/obj/Debug/net8.0/OrderManagement.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ba5dbec5bf9d7b0febb3082ae6c8dff3c558301")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
